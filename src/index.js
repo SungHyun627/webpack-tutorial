cf05b633ca60b1e6768542de4ae24566d33e02fe
Sunghyun627 <kksscc4@gmail.com>
2022-04-28 16:11:40 +0900

    chore: remove unnecessary library/setting and create project structure for authoring library

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import _ from 'lodash';
-import Print from './print';
+import numRef from './ref.json';
 
-async function getComponent() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  btn.innerHTML = 'Click me and check the console!';
-  element.appendChild(btn);
-
-  const { default: _ } = await import('lodash');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.onclick = Print.bind(null, 'Hello webpack!');
-  return element;
+export function numToWord(num) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.num === num ? ref.word : accum;
+    },
+    ''
+  );
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+export function wordToNum(word) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.word === word && word.toLowerCase() ? ref.num : accum;
+    },
+    -1
+  );
+}
