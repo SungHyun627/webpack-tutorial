123e0af2d492a532ec7aea624df2a7b7f5da2d2c
Sunghyun627 <kksscc4@gmail.com>
2022-04-28 14:50:13 +0900

    chore: apply dynamic imports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import _ from 'lodash';
 
-function component() {
+async function getComponent() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   btn.innerHTML = 'Click me and check the console!';
-
   element.appendChild(btn);
+  const { default: _ } = await import('lodash');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   return element;
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
