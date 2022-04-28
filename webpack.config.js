ccdcbc48ea59bd2a84ecd49cb5b92e9920cc74cb
Sunghyun627 <kksscc4@gmail.com>
2022-04-28 14:59:26 +0900

    chore: define the namesof output files using contenthash substitution

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Caching',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/',
